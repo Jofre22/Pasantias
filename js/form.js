0f927fe3ad71e31e6701efcfcbb6cbb525481918
Jofre22 <jofre22morocho@hotmail.com>
2016-09-05 22:38:52 -0500

    uso de bootstrap para diseño

--- js/form.js
+++ js/form.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
 
     $("input[name='myCheckbox']").on("click", function(){
         $tagMuestra = $(this);
-        $tagOculto= $tagMuestra.parent().find(".frutas");
+        $tagOculto= $tagMuestra.closest("tr").find(".frutas");
 
         if($tagMuestra.is(":checked")){
             $tagOculto.show();
         }else{
             $tagOculto.hide();
+
         }
     })
     var arreglo = [];
@@ -16,17 +17,16 @@ $(document).ready(function(){
 
     $(".enviar").on("click", function(){
         $(".frutas").each(function(){
-            $valor = $(this).val();
+            $valor = $(this).find("input").val();
             $nombre_fruta = $(this).parent().find(".nombre").attr("value");
             if($valor != ''){                
                 objeto_frutas[$nombre_fruta] = $valor;
-            }  
+            }
         });
         arreglo.push(objeto_frutas);
         console.log(arreglo);
     })    
 });
-
 
 
 
